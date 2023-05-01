ca47531e3710eaac2d756e6bf2772909c30ef7ef
Tredexi <nathadiel4@gmail.com>
2023-05-01 12:56:26 -0500

    nuvo comentario

--- src/gitvideo/ejemplogit.java
+++ src/gitvideo/ejemplogit.java
@@ -5,7 +5,7 @@ public class ejemplogit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola git");
-		
+	//hola
 		String nombre="Didier";
 	}
 
